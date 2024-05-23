d83d8498385e980331b1a41f850c7c9bbe3cb612
Hatem <64999740+iHGEN@users.noreply.github.com>
2024-05-23 14:22:09 +0300

    1.0v.js

---
+++ Data.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you nee
+      // TODO: Add SDKs for Firebase products that you want to use
+      // https://firebase.google.com/docs/web/setup#available-libraries
+    
+      // Your web app's Firebase configuration
+      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+      const firebaseConfig = {
+        apiKey: "AIzaSyBD-_20jCqt4S8tNu3nn4hdTPZZATOIfqA",
+        authDomain: "database-unity-test.firebaseapp.com",
+        databaseURL: "https://database-unity-test-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "database-unity-test",
+        storageBucket: "database-unity-test.appspot.com",
+        messagingSenderId: "295768757683",
+        appId: "1:295768757683:web:b6b8590a08f678a34c67c6",
+        measurementId: "G-ED2KRQSLBY"
+      };
+      firebase.initializeApp(firebaseConfig);
