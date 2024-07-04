afc862e8a77289c64477fb929c6fc2cd9da74ad6
Hatem <daimn.mekasa@gmail.com>
2024-07-04 11:48:18 +0300

    fix errors

--- ChckForUpdate.js
+++ ChckForUpdate.js
@@ -1,7 +1,7 @@
 const versionKey = 'GameVersion';
 const dbName = 'AraiCache';
 const storeName = 'Version';
-const newVersion = '1.1.2'; 
+const newVersion = '1.1.4'; 
 
 async function getDB() {
     return new Promise((resolve, reject) => {
