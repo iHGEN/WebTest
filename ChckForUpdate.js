3c2bdfa6a35346c30bfef4eefa54322e653ac879
Hatem <64999740+iHGEN@users.noreply.github.com>
2024-08-26 13:22:39 +0300

    Update ChckForUpdate.js

--- ChckForUpdate.js
+++ ChckForUpdate.js
@@ -1,7 +1,7 @@
 const versionKey = 'GameVersion';
 const dbName = 'AraiCache';
 const storeName = 'Version';
-const newVersion = '1.2.8'; 
+const newVersion = '1.3.1'; 
 
 async function getDB() {
     return new Promise((resolve, reject) => {
